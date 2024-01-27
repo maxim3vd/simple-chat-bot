036190e609cf86336986657f05b4f041336b823e
maxim3andr3 <maxim3.vd@gmail.com>
2024-01-27 13:05:57 +0100

    Remove extra lines

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 bot_name = "Codus"
 birth_year = 2024
 
-print(f"""
-Hello! My name is {bot_name}.
+print(f"""Hello! My name is {bot_name}.
 I was created in {birth_year}.""")
 
 user_name = input("Please, remind me your name.\n")
 print(f"What a great name you have, {user_name}!")
 
-print("""
-Let me guess your age.
+print("""Let me guess your age.
 Enter remainders of dividing your age by 3, 5 and 7.""")
 
 remainder_3 = int(input())
