76a20a0d6b4e9038d643d67eae149f1970228dc3
maxim3andr3 <maxim3.vd@gmail.com>
2024-01-27 12:23:32 +0100

    Ask user for their name and display it

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ print(f"""
 Hello! My name is {bot_name}.
 I was created in {birth_year}.
 """)
+
+user_name = input("Please, remind me your name.\n")
+print(f"What a great name you have, {user_name}!")
